d15cb5e6f318de869e7884d291e6ca874b2043bb
Simon Nielsen <simonrn@gmail.com>
2025-09-12 10:58:22 +0200

    Added Superclass for Animal-scripts

--- Assets/Core/Scripts/FlyingAnimalScript.cs
+++ Assets/Core/Scripts/FlyingAnimalScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FlyingAnimalScript : MonoBehaviour
+public class FlyingAnimalScript : AnimalBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Core/Scripts/SmallAnimalScript.cs
+++ Assets/Core/Scripts/SmallAnimalScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SmallAnimalScript : MonoBehaviour
+public class SmallAnimalScript : AnimalBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
