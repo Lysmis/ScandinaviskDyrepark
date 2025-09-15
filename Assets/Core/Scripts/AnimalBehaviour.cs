0d70f3a0342b13a95d8a7a98addb25abf1cbb30d
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-09-15 21:37:50 +0200

    Arbejder på AnimalBehavior script

--- Assets/Core/Scripts/AnimalBehaviour.cs
+++ Assets/Core/Scripts/AnimalBehaviour.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimalBehaviour : MonoBehaviour
 {
+    #region Field
+    //The jumping heigth - is public so it can bechanged in Unity
+    public float jumpHeigth = 5f;
+
+    //Input System Asset
+    public InputActionAsset inputActions;
+
+    //Animal rigidbody
+    private Rigidbody2D rb;
+
+    //Jump values
+    private InputAction jumpInput;
+    #endregion
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +30,51 @@ public class AnimalBehaviour : MonoBehaviour
     {
         
     }
+
+    private void Awake()
+    {
+        //Rigidbody
+        rb = GetComponent<Rigidbody2D>();
+
+        //Jumping input under the action "Player/Jump"
+        //Tap on touch screen and "Space" on keyboard
+        jumpInput = inputActions.FindActionMap("Player").FindAction("Jump");
+
+    }
+
+    private void OnEnable()
+    {
+        inputActions.FindActionMap("Player").Enable();
+
+        //Jump action
+        jumpInput.Enable();
+
+        //Staring jump
+        jumpInput.performed += ctx => Jumping();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Player").Disable();
+
+        jumpInput.Disable();
+
+        //Ending jump
+        jumpInput.performed -= ctx => Jumping();
+    }
+
+    private void Jumping()
+    {
+        Debug.Log("Hi");
+
+        Vector2 moveJump = transform.up * jumpHeigth * Time.deltaTime;
+
+        //Jump to new position
+        rb.MovePosition(rb.position + moveJump);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    Jumping();
+    //}
 }
