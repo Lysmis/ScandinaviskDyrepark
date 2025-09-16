60d1a98f850358f4fc60d6fdc5fdfde2ffab745b
Philip Eickmayer Hansen <philip-eh@hotmail.com>
2025-09-16 13:42:06 +0200

    Added HUD
    Made UI document with HUD that have labels to show counts of remaining seconds and picked up items.

--- Assets/Core/Scripts/HUDManager.cs
+++ Assets/Core/Scripts/HUDManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HUDManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Label timeLabel, pickUpLabel;
+
+    private void OnEnable()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        timeLabel = root.Q<Label>("TimeLabel");
+        pickUpLabel = root.Q<Label>("PickUpLabel");
+    }
+    /// <summary>
+    /// Sets the Time value in the HUD
+    /// </summary>
+    /// <param name="seconds">Number of seconds remaining</param>
+    public void SetTime(int seconds)
+    {
+        if (timeLabel != null)
+        {
+            timeLabel.text = $"{seconds}s";
+        }
+    }
+    /// <summary>
+    /// Sets the PickUp value in the HUD
+    /// </summary>
+    /// <param name="value">Number of items picked up by the player</param>
+    public void SetPickUp(int value)
+    {
+        if (pickUpLabel != null)
+        {
+            pickUpLabel.text = $"{value}";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
