5969e3c3e58270b2262fd5a4b1f041eb5b0747c7
Bjornlund <irenebjornlund@gmail.com>
2025-09-24 13:56:08 +0200

    Testing from scratch

--- Assets/Core/Scripts/CameraMove.cs
+++ Assets/Core/Scripts/CameraMove.cs
@@ -1,19 +1,3 @@
-//using UnityEngine;
-
-//public class CameraFollow : MonoBehaviour
-//{
-//    public Transform target; // dyret
-//    public Vector3 offset = new Vector3(0, 0, -10);
-
-//    void LateUpdate()
-//    {
-//        if (target != null)
-//        {
-//            transform.position = target.position + offset;
-//        }
-//    }
-//}
-
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
